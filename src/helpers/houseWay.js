ba97a55e4461f19ee56a56fd9482b41c5345b106
vibraniumforge <gregm9999@gmail.com>
2019-11-28 18:43:29 -0800

    houseway 1 pair works

--- src/helpers/houseWay.js
+++ src/helpers/houseWay.js
@@ -1,55 +1,64 @@
 const houseWay = hand => {
-  //   let highHand = [];
-  //   let lowHand = [];
+  let highHand = [];
+  let lowHand = [];
+  let finalHand = [];
 
   // check for pairs
 
-  let handResult = {};
-  for (let iterator in hand) {
-    if (handResult[iterator]) {
-      handResult[iterator] += 1;
+  debugger;
+
+  const sortedByPairHand = hand.slice().sort((a, b) => b.pairRank - a.pairRank);
+  let hasAPair = false;
+
+  // create object with amount of each pair, sorted ascending
+  let sortedHandResult = {};
+  for (let iterator of sortedByPairHand) {
+    if (sortedHandResult[iterator.pair_id]) {
+      sortedHandResult[iterator.pair_id] += 1;
     } else {
-      handResult[iterator] = 1;
+      sortedHandResult[iterator.pair_id] = 1;
     }
   }
-  console.log(handResult);
-  return handResult;
 
-  //   const cardObj = hand => {
-  //     return hand.reduce((cardObj, card) => {
-  //       if (cardObj[card.value]) {
-  //         cardObj[card.value]++;
-  //       } else {
-  //         cardObj[card.value] = 1;
-  //       }
-  //       return cardObj;
-  //     }, {});
-  //   };
-
-  //   for (let i = 0; i < hand.length; i++) {
-  //     for (let j = 1; j < hand.length; j++) {
-  //       if (hand[i].pair_id === hand[j].pair_id) {
-  //         for (let k = 0; k < hand.length; k++) {
-  //           if (k !== i && k !== j) {
-  //             lowHand.push(hand[k]);
-  //           }
-  //         }
-  //         highHand.push(hand[i], hand[j]);
-  //         return [[lowHand], [highHand]];
-  //       }
-  //     }
-  //   }
-  //   for (let l = 0; l < hand.length; l++) {
-  //     for (let m = 0; m < hand.length; m++) {
-  //       if (l.special) {
-  //         if (m.value === 7 || m.value === 8 || m.value === 9) {
-  //           highHand.push(hand[l]);
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return [[lowHand], [highHand]];
+  // if pair/pair, push lowest 2 into low, and highest 2 into high and return
+  if (
+    sortedHandResult.length === 2 &&
+    Object.values(sortedHandResult) === [2, 2]
+  ) {
+    lowHand.push(sortedByPairHand[0]);
+    lowHand.push(sortedByPairHand[1]);
+    highHand.push(sortedByPairHand[2]);
+    highHand.push(sortedByPairHand[3]);
+    finalHand.push(lowHand);
+    finalHand.push(highHand);
+    return finalHand;
+  } else {
+    onePair: for (let i = 0; i < 3; i++) {
+      for (let j = i + 1; j < 4; j++) {
+        if (sortedByPairHand[i].pair_id === sortedByPairHand[j].pair_id) {
+          console.log(sortedByPairHand[i]);
+          console.log(sortedByPairHand[j]);
+          highHand.push(sortedByPairHand.splice(j, j + 1)[0]);
+          highHand.push(sortedByPairHand.splice(i, i + 1)[0]);
+          console.log(sortedByPairHand);
+          hasAPair = true;
+          break onePair;
+        }
+      }
+    }
+    if (hasAPair) {
+      sortedByPairHand.forEach(tile => {
+        lowHand.push(tile);
+      });
+    }
+    console.log(sortedByPairHand);
+    console.log(lowHand);
+    console.log(highHand);
+    finalHand.push(lowHand);
+    finalHand.push(highHand);
+    console.log(finalHand);
+    return finalHand;
+  }
 };
 
 export { houseWay };
